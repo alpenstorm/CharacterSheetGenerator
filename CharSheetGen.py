183ebbc890248001f0560d41edf62354c872ab87
alpenstorm <69488574+alpenstorm@users.noreply.github.com>
2024-04-22 13:18:05 -0400

    update some global vars and functions

--- CharSheetGen.py
+++ CharSheetGen.py
@@ -10,10 +10,12 @@ import urllib.request
 # assign global variables 
 global saveFolder
 global tempFolder
+global textFolder
+
 global settings
 
 print('''**********************************************
-Character Sheet Generator (CSG) v.1.1.2
+Character Sheet Generator (CSG) v.1.1.3
 by alpenstorm
 **********************************************''')
 
@@ -26,7 +28,7 @@ by alpenstorm
 try:
     os.listdir("conf/")
 except:
-    print('NO CONFIG FOLDER FOUND AT conf/, CREATING...')
+    print("NO CONFIG FOLDER FOUND AT 'conf/', CREATING...")
     os.mkdir("conf/")
 
 # this one checks if the config file exists. if it doesn't, it will create one
@@ -35,10 +37,11 @@ try:
         settings = json.load(f)
         saveFolder = settings["saveFolder"]
         tempFolder = settings["tempFolder"]
+        textFolder = settings["textFolder"]
     f.close()
 except:
-    print('NO CONFIG FILE FOUND AT "conf/config.json, CREATING...')
-    data = {"saveFolder": "sheets/", "tempFolder": "temp/", "cssAsked": "false"}
+    print("NO CONFIG FILE FOUND AT 'conf/config.json', CREATING...")
+    data = {"saveFolder": "sheets/", "tempFolder": "temp/", "textFolder": "txt/", "cssAsked": "false"}
     with open('conf/config.json', "w", encoding='utf8') as w:
         w.write(json.dumps(data, indent=4))
     with open('conf/config.json', 'r', encoding='utf8') as r:
@@ -46,6 +49,8 @@ except:
 
     saveFolder = settings["saveFolder"]
     tempFolder = settings["tempFolder"]
+    textFolder = settings["textFolder"]
+
     w.close()
     r.close()
 
@@ -54,46 +59,43 @@ except:
 try:
     os.listdir(saveFolder)
     os.listdir(tempFolder)
+    os.listdir(f"{textFolder}/")
 except:
     print(f"NO SAVE FOLDER FOUND AT {saveFolder}, CREATING...")
     os.mkdir(saveFolder)
     print(f"NO TEMP FOLDER FOUND AT {tempFolder}, CREATING...")
     os.mkdir(tempFolder)
+    print(f"NO TEXT FOLDER FOUND AT '{textFolder}/', CREATING...")
+    os.mkdir(f"{textFolder}/")
 
 # this one tries to find the default csg files to add into the html
 try:
-    os.listdir("txt/")
-except:
-    print('NO TEXT FOLDER FOUND AT txt/, CREATING...')
-    os.mkdir("txt/")
-
-try:
-    with open('txt/basic.csg', 'r', encoding='utf8') as f:
-        file_basic = r'txt/basic.csg'
+    with open(f'{textFolder}/basic.csg', 'r', encoding='utf8') as f:
+        file_basic = f'{textFolder}/basic.csg'
         f.close()
-    with open('txt/other.csg', 'r', encoding='utf8') as f:
-        file_other = r'txt/other.csg'
+    with open(f'{textFolder}/other.csg', 'r', encoding='utf8') as f:
+        file_other = f'{textFolder}/other.csg'
         f.close()
-    with open('txt/combat.csg', 'r', encoding='utf8') as f:
-        file_combat = r'txt/combat.csg'
+    with open(f'{textFolder}/combat.csg', 'r', encoding='utf8') as f:
+        file_combat = f'{textFolder}/combat.csg'
         f.close()
-    with open('txt/quotes.csg', 'r', encoding='utf8') as f:
-        file_quotes = r'txt/quotes.csg'
+    with open(f'{textFolder}/quotes.csg', 'r', encoding='utf8') as f:
+        file_quotes = f'{textFolder}/quotes.csg'
         f.close()
-    with open('txt/trivia.csg', 'r', encoding='utf8') as f:
-        file_trivia = r'txt/trivia.csg'
+    with open(f'{textFolder}/trivia.csg', 'r', encoding='utf8') as f:
+        file_trivia = f'{textFolder}/trivia.csg'
         f.close()
 except:
-    print('NO BASIC TEXT FILE FOUND AT txt/basic.csg, CREATING...')
-    open('txt/basic.csg', 'w', encoding='utf8').close()
-    print('NO OTHER TEXT FILE FOUND AT txt/other.csg, CREATING...')
-    open('txt/other.csg', 'w', encoding='utf8').close()
-    print('NO COMBAT TEXT FILE FOUND AT txt/combat.csg, CREATING...')
-    open('txt/combat.csg', 'w', encoding='utf8').close()
-    print('NO QUOTES TEXT FILE FOUND AT txt/quotes.csg, CREATING...')
-    open('txt/quotes.csg', 'w', encoding='utf8').close()
-    print('NO TRIVIA TEXT FILE FOUND AT txt/trivia.csg, CREATING...')
-    open('txt/trivia.csg', 'w', encoding='utf8').close()
+    print(f"NO BASIC TEXT FILE FOUND AT '{textFolder}/basic.csg', CREATING...")
+    open(f'{textFolder}/basic.csg', 'w', encoding='utf8').close()
+    print(f"NO OTHER TEXT FILE FOUND AT '{textFolder}/other.csg', CREATING...")
+    open(f'{textFolder}/other.csg', 'w', encoding='utf8').close()
+    print(f"NO COMBAT TEXT FILE FOUND AT '{textFolder}/combat.csg', CREATING... ")
+    open(f'{textFolder}/combat.csg', 'w', encoding='utf8').close()
+    print(f"NO QUOTES TEXT FILE FOUND AT '{textFolder}/quotes.csg', CREATING... ")
+    open(f'{textFolder}/quotes.csg', 'w', encoding='utf8').close()
+    print(f"NO TRIVIA TEXT FILE FOUND AT '{textFolder}/trivia.csg', CREATING... ")
+    open(f'{textFolder}/trivia.csg', 'w', encoding='utf8').close()
 
     print("Input the basic info, other info, combat info, quotes, and trivia in these default files if you want. \nIf you don't, you can specify the text files in the prompt.")
     input("Restarting CSG, press [Return].")
@@ -104,19 +106,19 @@ except:
 #---------------------------------------------------
 
 # clears the terminal when called
-def clear():
+def clear() -> None:
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
 # creates a menu that downloads the default css file from my website and saves the response in the config json
-def css(message):
+def css(message: str) -> None:
     with open('conf/config.json', "r", encoding='utf8') as w:
         data = json.load(w)
         css_asked = data.get("cssAsked")
     w.close()
-    wr = {"saveFolder": "sheets/", "tempFolder": "temp/", "cssAsked": "true"}
+    wr = {"saveFolder": "sheets/", "tempFolder": "temp/", "textFolder": "txt/", "cssAsked": "true"}
     if css_asked == "false":
         if message == "y" or message == "Y":
             urllib.request.urlretrieve("https://dfstudios.neocities.org/charsheetmaker/styles.css", f"{saveFolder}/styles.css")
@@ -132,8 +134,17 @@ def css(message):
         else:
             css(input("[Y,N]: "))
 
+# asks to download a css file if the cssAsked variable is false in config.json
+def ask_css() -> None:
+    with open('conf/config.json', "r", encoding='utf8') as r:
+        data = json.load(r)
+        css_asked = data.get("cssAsked")
+        if css_asked == "false":
+            css(input("Do you want to download a CSS file for the style? [Y,N]: "))
+    r.close()
+
 # helper function for insert_br()
-def do_br(i: str, o: str):
+def do_br(i: str, o: str) -> None:
     try:
         replacements = {'\n': '<br>'}
         with open(i, 'r', encoding='utf8') as infile, open(o, 'w', encoding='utf8') as outfile:
@@ -149,14 +160,14 @@ def do_br(i: str, o: str):
         quit()    
 
 # takes an input txt file and creates a new text file with html <br> tags instead of newline chracters
-def insert_br(input_file_path="", default_input_file_path="", output_file_path=""):
+def insert_br(input_file_path: str = "", default_input_file_path: str = "", output_file_path: str = "") -> None:
     if default_input_file_path != "":
         do_br(default_input_file_path, output_file_path)
     else:
         do_br(input_file_path, output_file_path)
 
 # helper function for insert_li()
-def do_li(i: str ,o: str):
+def do_li(i: str ,o: str) -> None:
     try:
         with open(i, 'r') as input_file:
             lines = input_file.readlines()
@@ -172,14 +183,14 @@ def do_li(i: str ,o: str):
         quit()
 
 # takes an input txt file and creates an HTML list out of it
-def insert_li(input_file_path="txt/trivia.csg", default_input_file_path="", output_file_path=""):
+def insert_li(input_file_path: str = "", default_input_file_path: str = "", output_file_path: str = "") -> None:
     if default_input_file_path != "":
         do_li(default_input_file_path, output_file_path)
     else:
         do_li(input_file_path, output_file_path)
 
 # takes an input file, reads it, and stores the text in a variable
-def read_file_contents(file_path) -> str:
+def read_file_contents(file_path: str) -> str:
     try:
         with open(file_path, 'r', encoding='utf8') as file:
             file_contents = file.read()
@@ -188,7 +199,10 @@ def read_file_contents(file_path) -> str:
         return "File not found."
 
 # writes the input variables to an html file
-def write(img, maximgheight, name, origin, race, basicinfo, otherinfo, combatinfo, quotes, trivia):
+def write(img: str, maximgheight: int, name: str, origin: str, 
+          race: str, basicinfo: str, otherinfo: str, 
+          combatinfo: str, quotes: str, trivia: str) -> None:
+
     basicinfo = read_file_contents(f'{tempFolder}/basicInfoTemp.txt')
     otherinfo = read_file_contents(f'{tempFolder}/otherInfoTemp.txt')
     combatinfo = read_file_contents(f'{tempFolder}/combatInfoTemp.txt')
@@ -232,35 +246,28 @@ def write(img, maximgheight, name, origin, race, basicinfo, otherinfo, combatinf
     os.remove(f"{tempFolder}/triviaTemp.txt")
     print("Processed and removed temporary files!")
 
-def ask_css():
-    with open('conf/config.json', "r", encoding='utf8') as r:
-        data = json.load(r)
-        css_asked = data.get("cssAsked")
-        if css_asked == "false":
-            css(input("Do you want to download a CSS file for the style? [Y,N]: "))
-    r.close()
+# creates a small menu to quit the program
+def smallmenu(message: str) -> None:
+    if message == "y" or message == "Y":
+        clear()
+        main()
+    elif message == "n" or message == "N": quit()
+    else: smallmenu(input("[Y,N]: ")) 
 
 # puts it all together
-def main():
+def main() -> None:
     write(input("Enter the location of the image file for the char sheet (can be a web link): "), 
-          input("Enter the max image height (in pixels): "),
+          int(input("Enter the max image height (in pixels): ")),
           input("Enter the character's name: "), 
           input("Enter the character's origin: "),
           input("Enter the character's race: "),
-          insert_br(input("Enter text file containing basic info (default is txt/basic.csg): "), "txt/basic.csg", f"{tempFolder}/basicInfoTemp.txt"),
-          insert_br(input("Enter text file containing other info (default is txt/other.csg): "), "txt/other.csg", f"{tempFolder}/otherInfoTemp.txt"),
-          insert_br(input("Enter text file containing combat info (default is txt/combat.csg): "),"txt/combat.csg", f"{tempFolder}/combatInfoTemp.txt"),
-          insert_br(input("Enter text file containing quotes (default is txt/quotes.csg): "), "txt/quotes.csg", f"{tempFolder}/quotesTemp.txt"),
-          insert_li(input("Enter text file containing trivia (default is txt/trivia.csg): "), "txt/trivia.csg", f"{tempFolder}/triviaTemp.txt")
+          insert_br(input(f"Enter text file containing basic info (default is {textFolder}/basic.csg): "), f"{textFolder}/basic.csg", f"{tempFolder}/basicInfoTemp.txt"),
+          insert_br(input(f"Enter text file containing other info (default is {textFolder}/other.csg): "), f"{textFolder}/other.csg", f"{tempFolder}/otherInfoTemp.txt"),
+          insert_br(input(f"Enter text file containing combat info (default is {textFolder}/combat.csg): "),f"{textFolder}/combat.csg", f"{tempFolder}/combatInfoTemp.txt"),
+          insert_br(input(f"Enter text file containing quotes (default is {textFolder}/quotes.csg): "), f"{textFolder}/quotes.csg", f"{tempFolder}/quotesTemp.txt"),
+          insert_li(input(f"Enter text file containing trivia (default is {textFolder}/trivia.csg): "), f"{textFolder}/trivia.csg", f"{tempFolder}/triviaTemp.txt")
           )
-
-    xt = input("Do you want to create another sheet? [Y,N]: ")
-    
-    if xt == "y" or xt == "Y":
-        clear()
-        main()
-    elif xt == "n" or xt == "N": quit()
-    else: xt = input("[Y,N]: ") 
+    smallmenu(input("Do you want to create another sheet? [Y,N]: "))
 
 #---------------------------------------------------
 # Start the program and ask to download the CSS file
